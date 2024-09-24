9f57e5216eaec35c2fb709e3f83f0918d9caa84c
Jakob Visic <jakob.visic@gmail.com>
2024-09-24 08:16:30 -0400

    Update Hammer Bros.ino

--- Hammer Bros.ino
+++ Hammer Bros.ino
@@ -19,7 +19,7 @@ Jakob Visic
 OCADU DIGF-6037 Creation & Computation
 
 A 2 player game where the first player to score 3 points wins.
-Use your Hammer to hit the sensore to change your Orange lights brightness.
+Use your Hammer to hit the sensor to change your Orange lights brightness.
 Match your organge light's brightness with the brightness of the light in the center to score a point.
 There is a 1 second delay (on both players sides) after ever hit, so time your hits accordingly.
 Once a player wins, the lights will fade 3 times, all lights will light up, and then the game will restart.
